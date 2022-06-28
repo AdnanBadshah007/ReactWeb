d249f6c0c94b886c77cfe5f775ea06eca49d1848
Adnan <adnanbadshah28@gmail.com>
2022-06-28 22:10:47 +0530

    git

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 
-
-
 let Card = (props) => {
-    return (
-        <>
-            
-                <div className="col-md-4 col-10 mx-auto">
-                    <div className="card" >
-                    <img className="card-img-top" src={props.imgSrc} alt="Card image"/>
-                    <div className="card-body">
-                        <h4 className="card-title">John Doe</h4>
-                        <p className="card-text">Some example text.</p>
-                        <a href="#" className="btn btn-primary">See Profile</a>
-                    </div>
-                    </div>
-                </div>    
-        </>
-    )
-}
-
+  return (
+    <>
+      <div className="col-md-4 col-10 mx-auto">
+        <div className="card">
+          <img className="card-img-top" src={props.imgSrc} alt="Card" />
+          <div className="card-body">
+            <h4 className="card-title">John Doe</h4>
+            <p className="card-text">Some example text.</p>
+            <a href="/" className="btn btn-primary">
+              See Profile
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Card;
